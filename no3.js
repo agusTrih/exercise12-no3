0a62fbe799ad304ebf64afcfda13b2fd46540967
agusTrih <ant3dong@gmail.com>
2020-06-14 04:59:35 +0700

    siap publish

--- no3.js
+++ no3.js
@@ -1,18 +1,23 @@
+function ageCatConverter() {
+    let catAge = document.getElementById("catAge").value;
+    document.getElementById("show-converter").innerHTML = catAges(catAge);
+}
+// document.getElementById("show-converter").innerHTML = (catAge - 2) * 4 + 24;
 const catAges = (cat) => {
     const after2 = (cat - 2) * 4 + 24;
-    if (cat === 1) {
-        return `Umur kucing dalam kalender manusia 15 tahun`;
+    if (cat == 1) {
+        return `Kucing yang berusia 1 tahun, sebenarnya memiliki tubuh yang berusia 15 tahun dalam umur manusia`;
     } else if (cat === 2) {
-        return `Umur kucing dalam kalender manusia 24 tahun`;
+        return `Kucing yang berusia 1 tahun, sebenarnya memiliki tubuh yang berusia 24 tahun dalam umur manusia`;
     } else if (cat >= 2) {
-        return `Umur kucing dalam kalender manusia ${after2} tahun`;
+        return `Kucing yang berusia ${cat} tahun, sebenarnya memiliki tubuh yang berusia ${after2} tahun dalam umur manusia`;
     }
 };
 
-document.write(
-    catAges(
-        prompt(
-            "Berapa umur kucing yang igin kamu ketahui umurnya dalam kalender manusia: ketik Number!"
-        )
-    )
-);
+// document.write(
+//     catAges(
+//         prompt(
+//             "Berapa umur kucing yang igin kamu ketahui umurnya dalam kalender manusia: ketik Number!"
+//         )
+//     )
+// );
